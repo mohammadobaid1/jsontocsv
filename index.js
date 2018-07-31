9c612280ca509c13c2046a391b1998967faf0520
Muhammad Obaid <mobaid56@gmail.com>
2018-07-31 16:02:13 +0500

    node js code to convert fetched data first into json format and then into csv format for magmi plugin

---
+++ index.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var request =  require('request');
+var json2csv = require('json2csv').parse;
+var fields = ['sku','qty'];
+var app = express();
+
+
+
+const port = 8000;
+var cors = require('cors');
+app.options('*', cors());
+app.use(cors());
+
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.post('/fetchrecords',function(req,res){
+
+clientemail = req.body.email;
+clientsecret = req.body.clientsecret;
+
+request.post({url:"https://www.manchesterfurnituresupplies.co.uk/api/getallqty",form:{email:clientemail,secret_key:clientsecret}},function(err,httpResponse,body){
+
+  if(err){
+console.log('Error',err);
+
+}
+var jsonobject = JSON.parse(body);
+var rawdatainjson = jsonobject["data"];
+var finaljsondata = rawdatainjson.map(({sku, qty}) => ({sku, qty}));
+ const opts = { fields };
+try {
+  const csv = json2csv(finaljsondata, opts);
+  res.send(csv);
+} catch (err) {
+  console.error(err);
+
+}
+
+
+
+
+})
+
+
+
+});
+
+
+app.listen(port);
+
+
